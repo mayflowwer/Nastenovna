116d04ad65b7264bfdd6f57b2798a54ea285a078
mayflowwer <mayflowwer@gmail.com>
2022-01-20 17:19:38 +0200

    Исправлен пусть index.css в html-файле

---
+++ static/index.js
@@ -0,0 +1,11 @@
+console.log("I am running");
+window.onscroll = function() {myFunction()};
+var header = document.getElementById("myHeader");
+var sticky = header.offsetTop;
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
